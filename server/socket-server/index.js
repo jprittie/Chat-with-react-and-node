235fce6943cf0ce67c1ffa6e91e4d417b6909b6e
jprittie <eyewallop@gmail.com>
2017-10-26 21:53:07 -0400

    Project files added

---
+++ server/socket-server/index.js
@@ -0,0 +1,10 @@
+// Setup basic express server
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const port = process.env.PORT || 3000;
+
+server.listen(port, function() {
+  console.log("Server listening at port %d", port);
+});

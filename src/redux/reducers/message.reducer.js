8d686d738332e8ea384b4911c285fe71df88ae3a
jprittie <eyewallop@gmail.com>
2017-11-02 12:35:21 -0400

    Updates comments

--- src/redux/reducers/message.reducer.js
+++ src/redux/reducers/message.reducer.js
@@ -1,7 +1,8 @@
 import { INPUT_CHANGE, ADD_MESSAGE } from '../actions/message.actions';
 
 
-//This listens for the textInput to change
+//This listens for the textInput to change via a client side action
+//It is also listening for a server-side action of type 'clearInput' that will clear the textInput once the message is entered
 export const textInput = (state = '', action) => {
   switch (action.type) {
     case 'INPUT_CHANGE':
